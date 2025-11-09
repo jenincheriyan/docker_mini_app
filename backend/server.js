130f6114964ad1314ebc47fd6d70e9c410c359b9
jenin <jenincheriyan@gmail.com>
2025-11-09 16:50:22 +0530

    backend to cloud MongoDB

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 // Connect to MongoDB (container name: mongo)
-mongoose.connect('mongodb://mongo:27017/dockerlearn', {
+mongoose.connect(process.env.MONGO_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
